088f6b86ddd3622cf1711d82821e2caa48a77f64
funster-a <82862611+funster-a@users.noreply.github.com>
2024-03-03 14:42:13 +0500

    Edit playround function

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ const getComputerChoice = () => {
         };
 }
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "rock" && computerSelection === "scissors"){
+        return playerSelection;
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return computerSelection;
+    }
+    else if(playerSelection === "rock" && computerSelection === "rock"){
+        return false;
+    }
+    else if(playerSelection === "scissors" && computerSelection === "scissors"){
+        return false;
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return playerSelection;
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return computerSelection;
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return computerSelection;
+    }
+    else if(playerSelection === "paper" && computerSelection === "paper"){
+        return false;
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return playerSelection;
+    }
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
