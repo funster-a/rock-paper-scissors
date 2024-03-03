410ce8f708585a4666d8e0028a27613a108015f1
funster-a <82862611+funster-a@users.noreply.github.com>
2024-03-03 14:06:32 +0500

    Write first function getComputerChoice

---
+++ script.js
@@ -0,0 +1,16 @@
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "scissors"
+            break;
+        case 2:
+            return "paper"
+            break;
+         
+        };
+}
+console.log(getComputerChoice());
